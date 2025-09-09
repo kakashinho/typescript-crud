aea9ca825482a6d1dc7f0bd850066ac7614502d4
kakashinho <joaovitor.s.c.siqueira@gmail.com>
2025-09-08 22:07:50 -0300

    feat: inserindo o dotenv e inicialização com o banco de dados

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,11 +1,10 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { Post } from "./entities/Post";
-import { Category } from "./entities/Category";
-import * as dotenv from "dotenv"
-dotenv.config()
+import "reflect-metadata"// importação para o TypeORM funcionar com decoradores (@Entity, @Column, etc).
+import { DataSource } from "typeorm" // importamos o tipo principal do TypeORM
+import { User } from "./entities/User"; //Importamos a entidade de usuários
+import * as dotenv from "dotenv"//dotenv permite ler variáveis do .env
+dotenv.config()//arrega essas variáveis para que o código possa usá-las.
 
-
+//criar a conexão com o banco, chamada de AppDataSource.
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: process.env.DB_HOST!,
@@ -13,9 +12,17 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USERNAME!,
     password: process.env.DB_PASSWORD!,
     database: process.env.DB_NAME!,
-    synchronize: true,
-    logging: true,
-    entities: [Post, Category],
-    subscribers: [],
-    migrations: [],
+    synchronize: true, // Cria tabelas automaticamente (bom para desenvolvimento)
+    logging: true, //Mostra no terminal todos os comandos SQL executados
+    entities: [User], //Informa quais entidades (tabelas) o TypeORM deve usar.
+    subscribers: [],// eventos no banco, os "ouvintes"
+    migrations: [__dirname + "/migration/*.js"],//Informa onde estão os arquivos de migration
 })
+
+//iniciar a conexão com o banco de dados.
+AppDataSource.initialize()
+    .then(()  => {
+        console.log('Conexão Realizada com Sucesso!')
+    }).catch((error) => {
+        console.log('Erro na conexão com o banco de dados:', error)
+    })

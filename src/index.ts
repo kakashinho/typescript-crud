2d832ecbe76d49a26777e0bc3e4491a3e3000043
kakashinho <joaovitor.s.c.siqueira@gmail.com>
2025-09-08 22:09:21 -0300

    feat: inserindo o cors integrando o front end com o back end

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
+//biblioteca express, que serve para criar servidores.
 import express, {Request, Response} from "express";
-import { AppDataSource } from "./data-source";
 
+// Importar a biblioteca para permitir conexão externa, 
+// que permite o frontend (React/Next) se comunique com o backend (Express)
+import cors from 'cors'
+
+//aplicação Express, que é o servidor.
 const app = express();
 
-AppDataSource.initialize()
-    .then(()  => {
-        console.log('Conexão Realizada com Sucesso!')
-    }).catch((error) => {
-        console.log('Erro na conexão com o banco de dados:', error)
-    })
+//Recurso que diz: “esse servidor vai entender dados em formato JSON” (exemplo: { "nome": "João" }).
+app.use(express.json()); // Permite receber JSON
 
+// Criar o middleware para receber os dados no corpo da requisição
+app.use(cors());         // Permite chamadas do seu frontend para o seu backend.
+
+//Importando as rotas dos usuários, que ficam em outro arquivo (UsersController.ts)
+import UsersController from "./controllers/UsersController"
+
+// Ativa todas as rotas que você criou dentro de UsersController.
+app.use('/', UsersController) // Usa as rotas dos usuários
+
+ // Rota raiz
 app.get('/', (req: Request, res: Response) => {
     res.send('Bem-vindo')
-});
+})
 
+// Inicia servidor
 app.listen(8080, () => {
+    //listen(8080) faz o servidor "escutar" a porta 8080 (como uma porta de entrada).
     console.log('Servidor iniciado na porta 8080: http://localhost:8080')
-});
+})
